162aa1efcffd61d2c89edde87d19314b5ba73e10
John G' Rauh <jrrauh23@gmail.com>
2018-01-15 18:17:06 -0500

    'initialize server'

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ const app = express(); // creates an instance of an express application
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+app.use(function (req, res, next ) {
+  // do your logging here
+  // call `next`, or else your app will be a black hole — receiving requests but never properly responding
+
+})
+
