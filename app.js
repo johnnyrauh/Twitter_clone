ebbe38e6bd6061b316a0ad371bdfd967db3ffe8e
John G' Rauh <jrrauh23@gmail.com>
2018-01-16 11:26:03 -0500

    'starting github collab'

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
+// import { read } from 'fs';
+
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
+const morgan = require('morgan');
 
+app.use(morgan('dev'))
+ // do your logging here
+  // morgan('dev')
+  // call `n ext`, or else your app will be a black hole — receiving requests but never properly responding
+// )
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!')) // registering a listener for a current event
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
-app.use(function (req, res, next ) {
-  // do your logging here
-  // call `next`, or else your app will be a black hole — receiving requests but never properly responding
-
-})
 
